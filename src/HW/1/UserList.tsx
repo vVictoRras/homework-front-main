a6e4be73d0d4bd34e895d7928ecdf080834606ba
VictoRras <sirotilov@gmail.com>
2025-04-01 23:00:48 +0300

    hw2

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -11,14 +11,13 @@ type UserType = {
 };
 
 type UserListPropsType = {
-   users: UserType[];// ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+   users: UserType[]
 };
 
 export const UserList = (props: UserListPropsType) => {
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
-
       <ul>
         {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
           <li key={user.id} id={`hw01-user-${user.id}`}>
